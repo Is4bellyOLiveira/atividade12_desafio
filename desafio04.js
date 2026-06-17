72855af136c1be4078f73762258922f5eb3d3e59
Is4bellyOLiveira <isabellyoliveira18072008@gmail.com>
2026-06-17 14:24:06 -0300

    Add files via upload

---
+++ desafio04.js
@@ -0,0 +1,4 @@
+let numero1=Number(prompt("Digite o primeiro numero"));
+let numero2=Number(prompt("Digite o segundo numero"));
+if(numero1>numero2){alert(`O  numero ${numero1} e maior que numero ${numero2}`)}
+else{alert(`O numero ${numero2} e maior que o numero ${numero1}`)}
