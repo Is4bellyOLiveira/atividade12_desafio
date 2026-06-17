f28200f5289e3319a63dfda0005de1f293e415f4
Is4bellyOLiveira <isabellyoliveira18072008@gmail.com>
2026-06-17 15:34:42 -0300

    Add files via upload

---
+++ desafio06.js
@@ -0,0 +1,4 @@
+let idade=Number (prompt("digite a sua idade?"));
+let aluno=(prompt("vc é estudante?"));
+if (aluno==="sim" || idade>=60) {alert("Desconto: Meia-Entrada")}
+else {alert("Voce nao possui descontos")}
