2ef814adf19ef1067464689428ff3d8ba78ce932
Is4bellyOLiveira <isabellyoliveira18072008@gmail.com>
2026-06-17 14:55:57 -0300

    Add files via upload

---
+++ desafio05.js.js
@@ -0,0 +1,6 @@
+let usuario = (prompt("digite seu nome de usuario"));
+let senha = (prompt("digite sua senha"));
+if(usuario=="admin" && senha=="senai123"){alert("acesso concedido")}
+else(alert("acesso negado"));
+
+
