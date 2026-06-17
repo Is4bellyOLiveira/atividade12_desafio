31a051d00d7e4c0a2e783c3a740ed784461c518e
Is4bellyOLiveira <isabellyoliveira18072008@gmail.com>
2026-06-17 16:14:16 -0300

    Add files via upload

---
+++ desafio07.js
@@ -0,0 +1,10 @@
+let nota1=Number (prompt("escreva sua primeira nota "));
+let nota2=Number (prompt("escreva sua segunda nota "));
+let nota3=Number (prompt("escreva sua terceira nota "));
+const media=(nota1+nota2+nota3)/3
+if (media>=7.0){
+    alert("aprovado")}
+else if (media<=6.9){
+    alert("recuperacao")}
+else{alert("reprovado")}
+
